fd1305b91515c2edd49eb75683bc3181c2b88e8a
Geet <geetshelly2007@gmail.com>
2026-06-23 15:34:13 +0530

    remove build artifacts, add gitignore

--- build/drone_fleet/CMakeFiles/drone_demo.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for drone_demo.
-# This may be replaced when dependencies are built.
--- build/drone_fleet/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/geet/Downloads/fleet_ws/src/drone_fleet
-# Build directory: /home/geet/Downloads/fleet_ws/build/drone_fleet
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
